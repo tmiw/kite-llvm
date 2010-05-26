b81282f21cc6466f0485d53fb4c2ec6536991613
mooneer <mooneer@6f0f3af2-bb18-0410-b4af-aa22a44f8c3b>
2010-05-26 05:39:57 +0000

    forgot to add files
    git-svn-id: file:///home/svn/repos/kite/interpreter/branches/kite-llvm@612 6f0f3af2-bb18-0410-b4af-aa22a44f8c3b

---
+++ kite_type_thread.h
@@ -0,0 +1,53 @@
+/*****************************************************************************
+ * Copyright (c) 2010, Mooneer Salem
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Kite Language organization nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY MOONEER SALEM ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL MOONEER SALEM BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ****************************************************************************/
+
+#ifndef KITE_TYPE_THREAD_H
+#define KITE_TYPE_THREAD_H
+
+#include <pthread.h>
+#include "llvm/DerivedTypes.h"
+#include "llvm/LLVMContext.h"
+#include "llvm/Module.h"
+#include "llvm/Analysis/Verifier.h"
+#include "llvm/Support/IRBuilder.h"
+using namespace llvm;
+
+namespace kite
+{
+	namespace types
+	{
+		struct kite_thread_t
+		{
+			pthread_t *thread_pointer;
+			
+			static const Type* GetStructureType();
+			static const Type* GetPointerType();
+		};
+	}
+}
+
+#endif // KITE_TYPE_THREAD_H

f596c19cf3e5a99344b242b08643b64bf39277eb
Mooneer Salem <mooneer@gmail.com>
2012-12-26 19:10:18 -0800

    Fix compile error.

--- src/stdlib/System/string_type.cpp
+++ src/stdlib/System/string_type.cpp
@@ -24,7 +24,8 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  ****************************************************************************/
- 
+
+#include <stdio.h> 
 #include <iostream>
 #include <sstream>
 #include <boost/assign.hpp>
